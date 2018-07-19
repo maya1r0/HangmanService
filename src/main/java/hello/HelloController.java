55c4c8cac547e880876de83b1c3e1bcb8d9f7d81
Linde, Sophie <sophie.linde@cdk.com>
2018-07-19 14:49:59 -0700

    add database to service

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -2,10 +2,13 @@ package hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.sql.Connection;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class HelloController {
@@ -14,8 +17,20 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        jdbcTemplate.execute("insert into leaderboard values('name1', 0)");
+//        jdbcTemplate.execute("insert into leaderboard values('name1', 0)");
         return "Greetings from Spring Boot!";
     }
+    @RequestMapping("/get-leaderboard")
+    public List<Map<String,Object>> getLeaderboard() {
+        List<Map<String, Object>> maps = jdbcTemplate.queryForList("select * from  leaderboard");
+//        for(Map<String, Object> m: maps){
+//
+//        }
+        return maps;
+    }
+    @RequestMapping("/insert-into-leaderboard")
+    public void insertIntoLeaderboard(@RequestParam(value = "name")String name,@RequestParam(value = "score") int score) {
+        jdbcTemplate.execute("insert into leaderboard values('"+name+"', "+score+")");
+     }
     
 }
