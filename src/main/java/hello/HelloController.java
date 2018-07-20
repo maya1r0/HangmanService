bef7d3c1cd551cf864ad989da993ae3948cc105a
Rivera, Bella <Isabella.Rivera@cdk.com>
2018-07-20 09:16:05 -0700

    background adjusts to screen size

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -2,6 +2,7 @@ package hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
