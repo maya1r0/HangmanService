f437a420c48ce7a4dc301031ed2517cb39bb0f92
ryanm <mayasu.ryan@cdk.com>
2018-07-26 09:49:44 -0700

    added upStreaks method to the HelloController

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -18,12 +18,14 @@ public class HelloController {
     public String index() {
         return "Greetings from Spring Boot!";
     }
+
     @RequestMapping("/get-leaderboard")
     public List<Map<String,Object>> getLeaderboard() {
         List<Map<String, Object>> maps = jdbcTemplate.queryForList("select * from  leaderboard");
 
         return maps;
     }
+
     @RequestMapping("/insert-into-leaderboard")
     public void insertIntoLeaderboard(@RequestParam(value = "name")String name,@RequestParam(value = "score") int score) {
         jdbcTemplate.execute("insert into leaderboard values('"+name+"', "+score+")");
@@ -34,6 +36,20 @@ public class HelloController {
     public void storeUsername(@RequestParam(value = "name")String name) {
         jdbcTemplate.execute("insert into player_info values('"+name+"', 0, 0)");
     }
+
+    @CrossOrigin
+    @RequestMapping("/up-streaks")
+    public void upStreaks(@RequestParam(value = "name") String playerName) {
+        int currStreak = jdbcTemplate.queryForInt("select current_streak from player_info WHERE name = " + playerName) + 1;
+        int maxStreak = jdbcTemplate.queryForInt("select max_streak from player_info WHERE name = playerName");
+        if (currStreak >= maxStreak) {
+            jdbcTemplate.execute("UPDATE player_info SET current_streak =" + currStreak + ", max_streak = " + currStreak + "WHERE name = " + playerName);
+        }
+        else{
+            jdbcTemplate.execute("UPDATE player_info SET current_streak =" + currStreak + " WHERE name = " + playerName);
+        }
+    }
+
 
     @CrossOrigin
     @RequestMapping(value = "/hello", method = RequestMethod.GET, produces = "application/json")
